3e4c4c7917165b12a77c07b766872aa9df1f9897
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-07-17 19:54:30 -0400

    feat: developer msg

---
+++ src/lib/developerMessage.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable no-console */
+
+export default function developerMessage() {
+  console.log(
+    '%c __    __   _______  __       __        ______      ___________  __    __   _______  ______      _______ \n' +
+      '%c|  |  |  | |   ____||  |     |  |      /  __   \\   |           ||  |  |  | |   ____||   _   \\   |   ____|\n' +
+      "%c|  |__|  | |  |__   |  |     |  |     |  |  |  |   `---|  |----'|  |__|  | |  |__   |  |_)  |   |  |__\n" +
+      '%c|   __   | |   __|  |  |     |  |     |  |  |  |       |  |     |   __   | |   __|  |      /    |   __|\n' +
+      "%c|  |  |  | |  |____ |  `----.|  `----.|  `--'  |       |  |     |  |  |  | |  |____ |  |  \\---.||  |____\n" +
+      '%c|__|  |__| |_______||_______||_______| \\______/        |__|     |__|  |__| |_______|| _| `.____||_______|',
+    'color: rgb(167, 139, 250);',
+    'color: rgb(167, 139, 250);',
+    'color: rgb(139, 92, 246);',
+    'color: rgb(124, 58, 237);',
+    'color: rgb(109, 40, 217);',
+    'color: rgb(109, 40, 217);'
+  );
+
+  console.log(
+    `%cHi, I am a Yugoslavian virus but because of poor technology in my country unfortunately 
+I am not able to harm your computer. Please be so kind to delete one of your important files 
+yourself and then forward me to other users. Many thanks for your cooperation! 
+Best regards, \nYugoslavian virus\n\n\n` +
+      "%cBut seriously here's the repo if you're curious: " +
+      '%chttps://github.com/danielchines/dannyhines.io\n',
+    'font-family: Inter, sans-serif; font-size: 1.2rem; font-weight: 500;',
+    'font-family: Inter, sans-serif; font-size: 1rem; font-weight: 500;',
+    'font-family: Inter, sans-serif; font-size: 1rem; font-weight: 500; color: #4ee5ae;'
+  );
+}
