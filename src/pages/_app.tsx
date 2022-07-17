05c71953a1639b729ac31c1c5ed3317f4d21dd2e
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-07-17 12:52:18 -0400

    feat: chose primary color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,6 @@ import { ThemeProvider } from 'next-themes';
 
 import '@/styles/globals.css';
 
-/**
- * !STARTERCONF info
- * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
- */
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' defaultTheme='system'>
