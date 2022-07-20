4a77f686606200e9d9c2bff13991647beb2e731c
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-07-20 01:08:10 -0400

    fix: update to about

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
 import '@/styles/globals.css';
+import '@/styles/mdx.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import useLoaded from '@/hooks/useLoaded';
 
 import Layout from '@/components/layout/Layout';
-import UnderlineLink from '@/components/links/UnderlineLink';
+import PrimaryLink from '@/components/links/PrimaryLink';
 import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
 
@@ -21,23 +21,23 @@ export default function About() {
           )}
         >
           <div className='layout flex flex-col items-center justify-center gap-3 text-left sm:items-start'>
-            <h1 className='text-3xl' data-fade='1'>
+            <h1 className='mb-3 text-3xl sm:mb-1' data-fade='1'>
               Hi, I&apos;m{' '}
               <span className='text-primary-700 dark:text-primary-400'>
                 Danny
               </span>
             </h1>
 
-            <div className='flex w-full flex-col items-center  sm:flex-row'>
+            <div className='flex w-full flex-col items-center sm:flex-row'>
               <p
                 className='text-sm text-gray-700 dark:text-gray-300 sm:w-4/5 sm:text-base'
                 data-fade='2'
               >
                 I started coding in the Spring of my freshman year at college.
                 The language was Matlab, a mathy{' '}
-                <UnderlineLink href='https://www.mathworks.com/help/matlab/learn_matlab/matrices-and-arrays.html'>
+                <PrimaryLink href='https://www.mathworks.com/help/matlab/learn_matlab/matrices-and-arrays.html'>
                   matrix-based
-                </UnderlineLink>{' '}
+                </PrimaryLink>{' '}
                 language for handling data. It was unlike any other class I
                 took, and after I started watching Youtube videos on iOS
                 programming, I was hooked.
@@ -50,15 +50,16 @@ export default function About() {
                 <br />
                 Originally I made this site because I wanted to show off my side
                 projects. I&apos;ve also heard good things about{' '}
-                <UnderlineLink href='https://tailwindcss.com/'>
-                  TailWindCSS
-                </UnderlineLink>{' '}
+                <PrimaryLink href='https://tailwindcss.com/'>
+                  TailwindCSS
+                </PrimaryLink>{' '}
                 and I thought this would be a good way to try it out.
               </p>
               <NextImage
                 src='/images/me.jpg'
                 alt='Me'
                 className='float-right w-11/12 p-8 sm:ml-4 sm:w-2/5 sm:p-0'
+                data-fade='3'
                 imgClassName='rounded-md'
                 width={1200}
                 height={1117}
