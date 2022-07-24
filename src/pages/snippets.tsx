e090291173963680febf1513c3544a3af25ae96b
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-07-23 22:46:41 -0400

    feat: snippets

---
+++ src/pages/snippets.tsx
@@ -0,0 +1,54 @@
+import clsx from "clsx";
+import { InferGetStaticPropsType } from "next";
+import * as React from "react";
+
+import { getPostsByType } from "@/lib/mdx";
+import useLoaded from "@/hooks/useLoaded";
+
+import Layout from "@/components/layout/Layout";
+import Seo from "@/components/Seo";
+import SnippetCard from "@/components/SnippetCard";
+
+import { SnippetContent } from "@/types/Post";
+
+export default function ProjectsPage({
+  snippets,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const isLoaded = useLoaded();
+
+  return (
+    <Layout>
+      <Seo
+        templateTitle="Projects"
+        description="A collection of code snippets that I thought were worth saving."
+      />
+
+      <main>
+        <section
+          className={clsx("layout w-11/12 py-12", isLoaded && "fade-in-start")}
+        >
+          <h1 data-fade="1">Snippets</h1>
+          <p data-fade="2" className="mt-2 text-gray-600 dark:text-gray-300">
+            A collection of code snippets, settings and other pieces of code
+            that I thought were worth sharing.
+          </p>
+
+          <ul
+            data-fade="3"
+            className="mt-6 grid gap-4 sm:grid-cols-2 2xl:grid-cols-3"
+          >
+            {snippets.map((snippet: SnippetContent) => (
+              <SnippetCard key={snippet.slug} snippet={snippet} />
+            ))}
+          </ul>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const snippets = await getPostsByType("snippets");
+
+  return { props: { snippets } };
+}
---
+++ src/pages/snippets/[slug].tsx
@@ -0,0 +1,78 @@
+import { getMDXComponent } from "mdx-bundler/client";
+import { GetStaticPaths, GetStaticProps } from "next";
+import * as React from "react";
+
+import { getFileBySlug, getFiles } from "@/lib/mdx";
+
+import Layout from "@/components/layout/Layout";
+import CustomLink from "@/components/links/PrimaryLink";
+import MDXComponents from "@/components/MDXComponents";
+import Seo from "@/components/Seo";
+
+import { SnippetType } from "@/types/Post";
+
+export default function SnippetPage({ code, meta }: SnippetType) {
+  const Component = React.useMemo(() => getMDXComponent(code), [code]);
+
+  return (
+    <Layout>
+      <Seo
+        title={`${meta.title} – Danny Hines`}
+        description={meta.description}
+        type="article"
+      />
+
+      <main>
+        <section className="">
+          <div className="layout">
+            <h1 className="mt-8 lg:text-4xl">{meta.title}</h1>
+            <p className="my-6">{meta.description}</p>
+            <section>
+              <article className="snippets mdx dark:prose-dark prose mx-auto w-full transition-colors">
+                <Component
+                  components={
+                    {
+                      ...MDXComponents,
+                      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    } as any
+                  }
+                />
+              </article>
+            </section>
+
+            <div className="mt-8 flex flex-col items-start gap-4 md:flex-row-reverse md:justify-between">
+              <CustomLink
+                href={`https://github.com/theodorusclarence/theodorusclarence.com/blob/main/src/contents/snippets/${meta.slug}.mdx`}
+              >
+                Edit this on GitHub
+              </CustomLink>
+              <CustomLink href="/snippets">← Back to snippets</CustomLink>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getFiles("snippets");
+
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.mdx/, ""),
+      },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const post = await getFileBySlug("snippets", params?.slug as string);
+
+  return {
+    props: { ...post },
+  };
+};
