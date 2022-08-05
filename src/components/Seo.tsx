e998c60f2a3c5565e1b83cb3d4a3c584c41cca89
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-08-05 00:28:44 -0400

    refactor: remove og image and use me.jpg

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,18 +1,15 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-
-import { openGraph } from '@/lib/helper';
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 const defaultMeta = {
-  title: 'Danny Hines',
-  siteName: 'Danny Hines',
+  title: "Danny Hines",
+  siteName: "dannyhines.io",
   description:
-    'My personal website built with Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky.',
-  url: 'https://dannyhines.io',
-  type: 'website',
-  robots: 'follow, index',
-  /** This is populated with openGraph function */
-  image: '',
+    "My personal website built with Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky.",
+  url: "https://dannyhines.io",
+  type: "website",
+  robots: "follow, index",
+  image: "https://www.dannyhines.io/images/me.jpeg",
 };
 
 type SeoProps = {
@@ -26,46 +23,38 @@ export default function Seo(props: SeoProps) {
     ...defaultMeta,
     ...props,
   };
-  meta['title'] = props.templateTitle
+  meta["title"] = props.templateTitle
     ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
-
-  // Use siteName if there is templateTitle
-  // but show full title if there is none
-  meta['image'] = openGraph({
-    description: meta.description,
-    siteName: props.templateTitle ? meta.siteName : meta.title,
-    templateTitle: props.templateTitle,
-  });
 
   return (
     <Head>
       <title>{meta.title}</title>
-      <meta name='robots' content={meta.robots} />
-      <meta content={meta.description} name='description' />
-      <meta property='og:url' content={`${meta.url}${router.asPath}`} />
-      <link rel='canonical' href={`${meta.url}${router.asPath}`} />
+      <meta name="robots" content={meta.robots} />
+      <meta content={meta.description} name="description" />
+      <meta property="og:url" content={`${meta.url}${router.asPath}`} />
+      <link rel="canonical" href={`${meta.url}${router.asPath}`} />
       {/* Open Graph */}
-      <meta property='og:type' content={meta.type} />
-      <meta property='og:site_name' content={meta.siteName} />
-      <meta property='og:description' content={meta.description} />
-      <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content={meta.siteName} />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta name="image" property="og:image" content={meta.image} />
       {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' />
+      <meta name="twitter:card" content="summary_large_image" />
       {/* <meta name='twitter:site' content='@th_clarence' /> */}
-      <meta name='twitter:title' content={meta.title} />
-      <meta name='twitter:description' content={meta.description} />
-      <meta name='twitter:image' content={meta.image} />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:image" content={meta.image} />
       {meta.date && (
         <>
-          <meta property='article:published_time' content={meta.date} />
+          <meta property="article:published_time" content={meta.date} />
           <meta
-            name='publish_date'
-            property='og:publish_date'
+            name="publish_date"
+            property="og:publish_date"
             content={meta.date}
           />
-          <meta name='author' property='article:author' content='Danny Hines' />
+          <meta name="author" property="article:author" content="Danny Hines" />
         </>
       )}
 
@@ -73,12 +62,12 @@ export default function Seo(props: SeoProps) {
       {favicons.map((linkProps) => (
         <link key={linkProps.href} {...linkProps} />
       ))}
-      <meta name='msapplication-TileColor' content='#ffffff' />
+      <meta name="msapplication-TileColor" content="#ffffff" />
       <meta
-        name='msapplication-TileImage'
-        content='/favicon/ms-icon-144x144.png'
+        name="msapplication-TileImage"
+        content="/favicon/ms-icon-144x144.png"
       />
-      <meta name='theme-color' content='#ffffff' />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
@@ -92,76 +81,76 @@ type Favicons = {
 
 const favicons: Array<Favicons> = [
   {
-    rel: 'apple-touch-icon',
-    sizes: '57x57',
-    href: '/favicon/apple-icon-57x57.png',
+    rel: "apple-touch-icon",
+    sizes: "57x57",
+    href: "/favicon/apple-icon-57x57.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '60x60',
-    href: '/favicon/apple-icon-60x60.png',
+    rel: "apple-touch-icon",
+    sizes: "60x60",
+    href: "/favicon/apple-icon-60x60.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '72x72',
-    href: '/favicon/apple-icon-72x72.png',
+    rel: "apple-touch-icon",
+    sizes: "72x72",
+    href: "/favicon/apple-icon-72x72.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '76x76',
-    href: '/favicon/apple-icon-76x76.png',
+    rel: "apple-touch-icon",
+    sizes: "76x76",
+    href: "/favicon/apple-icon-76x76.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '114x114',
-    href: '/favicon/apple-icon-114x114.png',
+    rel: "apple-touch-icon",
+    sizes: "114x114",
+    href: "/favicon/apple-icon-114x114.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '120x120',
-    href: '/favicon/apple-icon-120x120.png',
+    rel: "apple-touch-icon",
+    sizes: "120x120",
+    href: "/favicon/apple-icon-120x120.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '144x144',
-    href: '/favicon/apple-icon-144x144.png',
+    rel: "apple-touch-icon",
+    sizes: "144x144",
+    href: "/favicon/apple-icon-144x144.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '152x152',
-    href: '/favicon/apple-icon-152x152.png',
+    rel: "apple-touch-icon",
+    sizes: "152x152",
+    href: "/favicon/apple-icon-152x152.png",
   },
   {
-    rel: 'apple-touch-icon',
-    sizes: '180x180',
-    href: '/favicon/apple-icon-180x180.png',
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/favicon/apple-icon-180x180.png",
   },
   {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
+    rel: "icon",
+    type: "image/png",
+    sizes: "192x192",
+    href: "/favicon/android-icon-192x192.png",
   },
   {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '32x32',
-    href: '/favicon/favicon-32x32.png',
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon/favicon-32x32.png",
   },
   {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
+    rel: "icon",
+    type: "image/png",
+    sizes: "96x96",
+    href: "/favicon/favicon-96x96.png",
   },
   {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '16x16',
-    href: '/favicon/favicon-16x16.png',
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon/favicon-16x16.png",
   },
   {
-    rel: 'manifest',
-    href: '/favicon/manifest.json',
+    rel: "manifest",
+    href: "/favicon/manifest.json",
   },
 ];
