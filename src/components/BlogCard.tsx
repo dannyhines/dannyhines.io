1d840afed55b1a6f316fd0be57a6ae30b40df201
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-08-09 01:04:07 -0400

    style: better blog card

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -15,7 +15,7 @@ export default function BlogCard({ blog, className }: BlogCardProps) {
   return (
     <li
       className={clsx(
-        'rounded-lg md:w-full',
+        'project-card rounded-lg md:w-full',
         'border dark:border-gray-600',
         'scale-100 hover:scale-[1.02] active:scale-[0.97] motion-safe:transform-gpu',
         'transition duration-100',
@@ -26,31 +26,26 @@ export default function BlogCard({ blog, className }: BlogCardProps) {
     >
       <UnstyledLink
         href={`/blog/${blog.slug}`}
-        className='flex flex-col items-center rounded-md p-4 focus:outline-none focus-visible:ring focus-visible:ring-primary-300 sm:flex-row'
+        className='flex flex-col items-center rounded-md focus:outline-none focus-visible:ring focus-visible:ring-primary-300 sm:flex-row'
       >
-        <div className='mb-4 w-1/2 rounded-md sm:m-0 sm:w-1/3 md:w-1/4 lg:w-1/6 '>
+        <div className='relative h-full w-full sm:m-0 md:w-[220px]'>
           <NextImage
             layout='responsive'
-            className='pointer-events-none w-full rounded-md'
-            imgClassName='rounded-md'
+            className='pointer-events-none h-full w-full'
+            imgClassName='sm:rounded-bl-md sm:rounded-tr-none rounded-tr-md rounded-tl-md rounded-bl-none'
             src={`/images/${blog.slug}/header.png`}
             alt={blog.title}
-            width={120}
-            height={70}
-            sizes='20vw'
-            // preview={false}
+            width={300}
+            height={200}
           />
         </div>
-        <div className='ml-4 flex flex-col items-start justify-start'>
+        <div className='ml-4 flex flex-col items-start justify-start p-4'>
           <h4 className='mb-2'>{blog.title}</h4>
           <p className='mb-auto text-sm text-gray-700 dark:text-gray-300'>
             {blog.description}
           </p>
-          {/* <div className='mt-2'>
-          <TechIcons techs={blog.tags.split(',') as string[]} />
-        </div> */}
 
-          <p className='animated-underline mt-2 inline-block font-medium'>See more →</p>
+          <p className='animated-underline mt-2 inline-block font-medium'>Read more →</p>
         </div>
       </UnstyledLink>
     </li>
