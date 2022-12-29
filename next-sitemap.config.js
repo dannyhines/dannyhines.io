21c1a0de1160051277e00389b0284914b8a9285a
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2022-12-29 17:40:26 -0600

    fix: harambe in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,7 @@
  */
 module.exports = {
   siteUrl: 'https://www.dannyhines.io',
+  exclude: '/harambe',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
