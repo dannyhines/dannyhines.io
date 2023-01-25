16c64c35736370cb680b2189b00da055d44759ec
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2023-01-25 00:40:39 -0500

    feat: footer accounts for mobile, dark theme

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,48 +8,67 @@ export default function Footer() {
     <>
       <footer className='pt-8 pb-6 '>
         <div className='layout mx-auto'>
-          <hr className='my-6' />
+          <hr className='my-6 dark:border-gray-700' />
 
           <div className='mx-auto flex flex-wrap justify-center gap-x-8 gap-y-4 py-4 md:w-10/12 md:justify-between'>
-            <UnstyledLink href='/about'>About</UnstyledLink>
-            <UnstyledLink href='/blog'>Blog</UnstyledLink>
-            <UnstyledLink href='/projects'>Projects</UnstyledLink>
-            <UnstyledLink href='/snippets'>Snippets</UnstyledLink>
-            <UnstyledLink href='https://github.com/dannyhines/dannyhines.io'>
+            <UnstyledLink href='/about' className='dark:opacity-80 dark:hover:opacity-100'>
+              About
+            </UnstyledLink>
+            <UnstyledLink href='/blog' className='dark:opacity-80 dark:hover:opacity-100'>
+              Blog
+            </UnstyledLink>
+            <UnstyledLink
+              href='/projects'
+              className='dark:opacity-80 dark:hover:opacity-100'
+            >
+              Projects
+            </UnstyledLink>
+            <UnstyledLink
+              href='/snippets'
+              className='dark:opacity-80 dark:hover:opacity-100'
+            >
+              Snippets
+            </UnstyledLink>
+            <UnstyledLink
+              href='https://github.com/dannyhines/dannyhines.io'
+              className='dark:opacity-80 dark:hover:opacity-100'
+            >
               Source code
             </UnstyledLink>
-            <UnstyledLink href='mailto:royalty_pitons_0v@icloud.com'>
+            <UnstyledLink
+              href='mailto:royalty_pitons_0v@icloud.com'
+              className='dark:opacity-80 dark:hover:opacity-100'
+            >
               Contact me
             </UnstyledLink>
           </div>
 
-          <div className='w-full flex-col py-8 text-center'>
-            {/* <h4 className='md:text-2xl font-semibold'>Let&apos;s connect</h4> */}
-            <h5 className='my-4 text-xl font-semibold text-gray-700'>Let&apos;s connect</h5>
+          <div className='w-full flex-col py-6 text-center'>
+            {/* <h5 className='my-4 text-xl font-semibold opacity-90'>Let&apos;s connect</h5> */}
             <div className='flex justify-center gap-4'>
               <PrimaryLink
-                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal text-black shadow-lg outline-none focus:outline-none md:h-12 md:w-12'
+                className='align-center dark:text-red h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal  shadow-lg outline-none focus:outline-none dark:bg-gray-800 md:h-12 md:w-12'
                 href='https://github.com/dannyhines'
                 aria-label='View my Github profile'
               >
-                <SiGithub className='text-xl text-black md:text-2xl' />
+                <SiGithub className='text-xl text-black dark:text-gray-200 md:text-2xl' />
               </PrimaryLink>
               <PrimaryLink
-                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal text-blue-400 shadow-lg outline-none focus:outline-none md:h-12 md:w-12'
+                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal  shadow-lg outline-none focus:outline-none dark:bg-gray-800 md:h-12 md:w-12'
                 href='https://twitter.com/dannyhines_'
                 aria-label='View my Twitter profile'
               >
                 <SiTwitter className='text-xl text-blue-400 md:text-2xl' />
               </PrimaryLink>
               <PrimaryLink
-                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal text-blue-600 shadow-lg outline-none focus:outline-none md:h-12 md:w-12'
+                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal shadow-lg outline-none focus:outline-none dark:bg-gray-800 md:h-12 md:w-12'
                 href='https://www.linkedin.com/in/danny-hines-b10483121/'
                 aria-label='View my LinkedIn profile'
               >
                 <SiLinkedin className='text-xl text-blue-600 md:text-2xl' />
               </PrimaryLink>
               <PrimaryLink
-                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal text-red-600 shadow-lg outline-none focus:outline-none md:h-12 md:w-12'
+                className='align-center h-10 w-10 items-center justify-center rounded-full bg-white p-2 font-normal  shadow-lg outline-none focus:outline-none dark:bg-gray-800 md:h-12 md:w-12'
                 href='mailto:royalty_pitons_0v@icloud.com'
                 aria-label='Send me an email'
               >
@@ -58,7 +77,7 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className='mt-8 mb-8 text-center text-sm text-gray-400'>
+          <div className='mt-4 mb-10 text-center text-xs text-gray-400 dark:text-gray-500'>
             Copyright © {new Date().getFullYear()} Danny Hines
           </div>
         </div>
